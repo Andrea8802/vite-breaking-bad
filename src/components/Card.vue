f658e93b3d96f6f99be94f8085c12102856a8162
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-14 16:02:11 +0100

    Creato template card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,10 +10,16 @@ export default {
     <div class="card">
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="">
         <div class="name">
-
+            Mario
         </div>
-        <div class="status"></div>
-        <div class="species"></div>
+        <div class="info">
+            <div class="status">
+                Dead
+            </div>
+            <div class="species">
+                Human
+            </div>
+        </div>
     </div>
 </template>
 
@@ -22,7 +28,31 @@ export default {
 
 .card {
     height: 300px;
-    width: 150px;
+    width: 180px;
     background-color: $primary-color;
+    text-align: center;
+    padding: 15px;
+
+    img {
+        width: 100%;
+    }
+
+    .name {
+        color: white;
+        font-weight: bold;
+        margin-top: 10px;
+    }
+
+    .info {
+        margin-top: 15px;
+
+        .status,
+        .species {
+            color: gray;
+            margin-top: 5px;
+        }
+    }
+
+
 }
 </style>
